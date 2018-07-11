6d76f3fd8f23411a66ded34782d9a4787c2b7cc1
AnaSanchezJi <anpasanchezji@ittepic.edu.mx>
2018-07-11 08:00:33 -0600

    Ejercicio 1

---
+++ mainlibro.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+var userSchema = require('./libroSchema');
+
+//Conexión
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/libros',{useMongoClient:true});
+
+//Modelo
+//parametros('como sera llamado en js',esquema,'como se llama la colleción en mongo')
+var Libro = mongoose.model('Libro',userSchema,'libro');
+
+var libro= new Libro({
+    title: "Antes de ti",
+    author: "Yoyo",
+    body: "Historia de drama-romance"
+});
+
+libro.save(function(error){
+    if(erro){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("Guardado");
+    process.exit(0);
+});
